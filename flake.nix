f26aef470757ab8a8490a8f34c348816271e37a6
Adithya Nair <adtya@adtya.xyz>
2024-05-24 17:23:15 +0530

    init

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Varnam on Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        packages = rec {
+        };
+      }
+    );
+}
